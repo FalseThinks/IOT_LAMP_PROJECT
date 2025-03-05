c14b08a1e50f850f929f7ed0fe69d7e97e9a643b
paproka <alarcon590@gmail.com>
2025-03-05 14:47:31 +0100

    IOT Lamp Project 1.2.0 - MQTT (missing JSON)

--- lib/MQTT_Broker/mqtt_broker.c
+++ lib/MQTT_Broker/mqtt_broker.c
@@ -3,44 +3,146 @@
 
 static const char *TAG = "MQTT_CLIENT";
 
-#define DEFAULT_STACKSIZE 4096     // Stack size needed for MQTT tasks
+#define DEFAULT_STACKSIZE 4096                         // Stack size needed for MQTT tasks
 #define DEFAULT_PRIORITY tskIDLE_PRIORITY              // Lowest possible task priority
 #define DEFAULT_TICK_DELAY (1000 / portTICK_PERIOD_MS) // Time for delay
 static TaskHandle_t xHandle = NULL;                    // Task handle
 
 static bool mqtt_client_already_started = false;
+static int num_leds;
 
-void task(void *pvParameters)
+static led_status_json_t *leds_json;
+
+static bool isIntermitent = false;
+static bool isFlush = false;
+static uint8_t global_brightness = -1;
+
+void init_led_json() {
+    leds_json = malloc(sizeof(led_status_json_t)*num_leds);
+    for (int i = 0; i < num_leds; i++) {
+        leds_json[i].hex_color = -1;   // Default not set
+        leds_json[i].brightness = 255;   // Default to full brightness
+    }
+}
+
+void get_data_json(const char *json_string)
+{
+    cJSON *root = cJSON_Parse(json_string);
+    if (!root) return;
+
+    cJSON *led_numbers = cJSON_GetObjectItem(root, "leds");
+    if (led_numbers) {
+        for (int i = 0; i < num_leds; i++) {
+            char index_str[12]; //3????
+            snprintf(index_str, sizeof(index_str), "%d", i);
+
+            cJSON *led = cJSON_GetObjectItem(led_numbers, index_str);
+            if (led) {
+                // Update color
+                cJSON *color = cJSON_GetObjectItem(led, "c");
+                if (color && cJSON_IsString(color) && color->valuestring[0] == '#') {
+                    leds_json[i].hex_color = (uint32_t)strtol(color->valuestring + 1, NULL, 16);
+                }
+
+                // Update brightness
+                cJSON *brightness = cJSON_GetObjectItem(led, "b");
+                if (brightness && cJSON_IsString(brightness)) {
+                    leds_json[i].brightness = (uint8_t)atoi(brightness->valuestring);
+                }
+            }
+        }
+    }
+    cJSON *bool_is_flush = cJSON_GetObjectItem(root, "is_flush");
+    isFlush = cJSON_IsTrue(bool_is_flush);
+    cJSON *bool_is_intermitent = cJSON_GetObjectItem(root, "is_intermitent");
+    isIntermitent = cJSON_IsTrue(bool_is_intermitent);
+    cJSON *global_brightness_value = cJSON_GetObjectItem(root, "global_b");
+    global_brightness = (cJSON_IsString(global_brightness_value)) ? atoi(global_brightness_value->valuestring) : -1;
+
+    cJSON_Delete(root);
+}
+
+void restartTask(void *pvParameters)
 {
     esp_mqtt_client_handle_t client = (esp_mqtt_client_handle_t)pvParameters;
+    int retry_count = 0;
+    const int max_retries = 5; // Number of retries before giving up
 
     esp_mqtt_client_stop(client);
-    mqtt_client_already_started = false;
-    // Wait for a short delay (e.g., 5 seconds)
-    vTaskDelay(5000 / portTICK_PERIOD_MS);
 
-    // Restart the MQTT client
-    ESP_LOGI(TAG, "Restarting MQTT client...");
-    esp_mqtt_client_start(client);
-    mqtt_client_already_started = true;
+    while (retry_count < max_retries)
+    {
+        vTaskDelay(DEFAULT_TICK_DELAY); // Wait before retrying
 
-    // Delete the task after completion
+        ESP_LOGI(TAG, "Restarting MQTT client (Attempt %d/%d)...", retry_count + 1, max_retries);
+        esp_err_t err = esp_mqtt_client_start(client);
+
+        if (err == ESP_OK)
+        {
+            ESP_LOGI(TAG, "MQTT client restarted successfully!");
+            mqtt_client_already_started = true;
+            break; // Exit retry loop
+        }
+        else
+        {
+            ESP_LOGE(TAG, "MQTT restart failed, retrying...");
+            retry_count++;
+        }
+    }
+
+    if (retry_count == max_retries)
+    {
+        ESP_LOGE(TAG, "MQTT restart failed after %d attempts. Giving up.", max_retries);
+        esp_restart();
+    }
+
     vTaskDelete(NULL);
 }
 
-void createNewRestartTask(const char *const TASK_NAME, esp_mqtt_client_handle_t param, TaskHandle_t *xHandle)
+void loopTask(void *pvParameters)
 {
-    BaseType_t xReturned;
+    //TBD
+}
 
-    // Create the task, storing the handle.
-    xReturned = xTaskCreate(
-        task,              // Function that implements the task.
-        TASK_NAME,         // Text name for the task.
-        DEFAULT_STACKSIZE, // Stack size in words, not bytes.
-        (void *)param,     // Parameter passed into the task.
-        DEFAULT_PRIORITY,  // Priority at which the task is created.
-        xHandle);          // Used to pass out the created task's handle.
-                           // We are passing it as a pointer, so is not neccesary anymore.
+void createNewTask(const char *const TASK_NAME, esp_mqtt_client_handle_t param, TaskHandle_t *xHandle, task_type_t taskType)
+{
+    //Initialization
+    BaseType_t xReturned = pdFALSE;
+
+    if (*xHandle != NULL) {
+        printf("Task already exists. Deleting it...\n");
+        vTaskDelete(*xHandle);
+        *xHandle = NULL;
+    }
+
+    switch (taskType)
+    {
+    case RESTART_TASK:
+    {
+        // Create the task, storing the handle.
+        xReturned = xTaskCreate(
+            restartTask,              // Function that implements the task.
+            TASK_NAME,         // Text name for the task.
+            DEFAULT_STACKSIZE, // Stack size in words, not bytes.
+            (void *)param,     // Parameter passed into the task.
+            DEFAULT_PRIORITY,  // Priority at which the task is created.
+            xHandle);          // Used to pass out the created task's handle.
+                               // We are passing it as a pointer, so is not neccesary anymore
+        break;
+    }
+    case LOOP_TASK:
+    {
+        xReturned = xTaskCreate(
+            loopTask,
+            TASK_NAME,         
+            DEFAULT_STACKSIZE, 
+            (void *)param,     
+            DEFAULT_PRIORITY,  
+            xHandle);          
+                               
+        break;
+    }
+    }
 
     if (xReturned == pdPASS)
     {
@@ -54,37 +156,83 @@ void createNewRestartTask(const char *const TASK_NAME, esp_mqtt_client_handle_t 
     }
 }
 
+void apply_sent_data()
+{
+    for (int i=0; i<num_leds; i++)
+    {
+        if(leds_json[i].hex_color!=-1)
+            setHEX(i,leds_json[i].hex_color);
+    }
+}
+
 static void handle_topic(const char *full_topic, const char *subtopic,
                          const char *data, esp_mqtt_client_handle_t client)
 {
-    ESP_LOGI(TAG, "Performing action for subtopic: %s, data: %s", subtopic, data);
-
+    //ESP_LOGI(TAG, "Performing action for subtopic: %s, data: %s", subtopic, data);
+    
+    vTaskDelay(DEFAULT_TICK_DELAY);
     if (strcmp(subtopic, DEFAULT_FUNCTION) == 0)
     {
+        
         if (strcmp(data, SPANISH_FLAG) == 0)
         {
+            clearLeds();
+            sendData();
             spanish_flag();
         }
-        else if (strcmp(data, ANDALUSIA_FLAG) == 0)
+        else if (strcmp(data, ITALIAN_FLAG)==0)
         {
+            clearLeds();
+            sendData();
+            italian_flag();
+        }
+        else if (strcmp(data, ANDALUSIAN_FLAG) == 0)
+        {
+            clearLeds();
+            sendData();
             andalusia_flag();
         }
         else if (strcmp(data, HAPPY_FACE) == 0)
         {
+            clearLeds();
+            sendData();
             happy_face();
         }
         else if (strcmp(data, RAINBOW) == 0)
         {
+            clearLeds();
+            sendData();
             rainbow();
         }
         else
         {
-            // HANDLE WITH DATA INPUTS
+            const char *base_topic_full_to_empty = "full_to_empty/color=";
+
+            if (strncmp(data, base_topic_full_to_empty, strlen(base_topic_full_to_empty)) == 0)
+            {
+                const char *HEX_full_to_empty_char = data + strlen(base_topic_full_to_empty);     
+                const char* hex_without_hash = HEX_full_to_empty_char + 1;
+                printf("PRE: %s",hex_without_hash);
+                uint32_t HEX_value = (uint32_t)strtol(hex_without_hash, NULL, 16);
+                printf("POST: %li",HEX_value);
+                //createNewTask("FULL_TO_EMPTY_TASK", )
+                full_to_empty(HEX_value);
+            }
+            else
+            {
+                ESP_LOGW(TAG, "Received data does not match expected. See MQTT_BROKER.H");
+                return;
+            }
         }
     }
-    else
-    { // Other stuff
-    }
+    else if (strcmp(subtopic, JSON_FUNCTION) == 0)
+    { 
+        ESP_LOGI(TAG, "Performing JSON action");
+        get_data_json(data);
+        apply_sent_data();
+        sendData();
+    }else
+        ESP_LOGE(TAG, "NOT EXPECTED FORMAT");
 }
 
 static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_t event_id, void *event_data)
@@ -104,12 +252,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 
     case MQTT_EVENT_DISCONNECTED:
         ESP_LOGW(TAG, "MQTT_EVENT_DISCONNECTED");
-        if (mqtt_client_already_started)
-        {
-            mqtt_client_already_started = false;
-            createNewRestartTask("RESTART_MQTT", client, &xHandle);
-        }
-
+        createNewTask("RESTART_MQTT", client, &xHandle, RESTART_TASK);
+        vTaskDelay(DEFAULT_TICK_DELAY);  
         break;
 
     case MQTT_EVENT_ERROR:
@@ -117,7 +261,8 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
         if (mqtt_client_already_started)
         {
             mqtt_client_already_started = false;
-            createNewRestartTask("RESTART_MQTT", client, &xHandle);
+            createNewTask("RESTART_MQTT", client, &xHandle, RESTART_TASK);
+            vTaskDelay(DEFAULT_TICK_DELAY);
         }
         break;
 
@@ -142,9 +287,19 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
             return;
         }
 
+        char *data;
+        size_t buffer = 1;
         // Extract the data
-        char data[100];
-        snprintf(data, sizeof(data), "%.*s", event->data_len, event->data);
+        if (strcmp(subtopic, JSON_FUNCTION)==0){
+            buffer = 1000;
+            data = malloc(buffer);
+        }
+        else{
+            buffer = 250;
+            data = malloc(buffer);
+        }     
+
+        snprintf(data, buffer, "%.*s", event->data_len, event->data);
         ESP_LOGI(TAG, "Data: %s", data);
 
         handle_topic(full_topic, subtopic, data, client);
@@ -179,7 +334,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
     }
 }
 
-void mqtt_init(char *BROKER_URI, char *AUTH_USR, char *AUTH_PWD)
+void mqtt_init(char *BROKER_URI, char *AUTH_USR, char *AUTH_PWD, int numLeds)
 {
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker = {
@@ -191,7 +346,10 @@ void mqtt_init(char *BROKER_URI, char *AUTH_USR, char *AUTH_PWD)
         .network.disable_auto_reconnect = true, // Disable auto-reconnect
         .network.timeout_ms = 300               // Reduce keep-alive timeout
     };
-
+    ESP_LOGI(TAG, "STARTING MQTT CONFIGURATION");
+    num_leds = numLeds;
+    init_led_json();
+    vTaskDelay(DEFAULT_TICK_DELAY);
     esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
     esp_mqtt_client_register_event(client, ESP_EVENT_ANY_ID, mqtt_event_handler, NULL);
     esp_mqtt_client_start(client);
--- lib/MQTT_Broker/mqtt_broker.h
+++ lib/MQTT_Broker/mqtt_broker.h
@@ -1,6 +1,19 @@
 #include "Paproka_LED_Lamp.h"
 #include "LED_Default_Functions.h"
 #include "mqtt_client.h"
+#include <cJSON.h>
+
+typedef enum
+{
+    RESTART_TASK,
+    LOOP_TASK
+} task_type_t;
+
+typedef struct
+{
+    uint32_t hex_color;
+    uint8_t brightness;
+} led_status_json_t;
 
 #define TOPIC_BASE_URI_NO_WILDCARD "topic/lamp/"
 // # is used as wildcard for receiving new messages
@@ -8,34 +21,59 @@
 
 //-- DEFAULT FUNCTIONS
 
-//Example: lamp/default_functions/ + flush/flush_one/color=HEX
+// Example: lamp/default_functions/ + flush/flush_one/color=HEX
 #define DEFAULT_FUNCTION "default_functions/"
-
 #define SPANISH_FLAG "flags/spanish_flag"
-#define ANDALUSIA_FLAG "flags/andalusian_flag"
+#define ANDALUSIAN_FLAG "flags/andalusian_flag"
 #define ITALIAN_FLAG "flags/italian_flag"
-
 #define HAPPY_FACE "happy_face"
 #define RAINBOW "rainbow"
-
-#define FULL_TO_EMPTY "full_to_empty/color=HEX"
-
-#define FLUSH_ONE "flush/flush_one/color=HEX&amount=4"
-#define FLUSH_TWO "flush/flush_two/color1=HEX&color2=HEX2&amount=4"
-#define FLUSH_THREE "flush/flush_three/color1=HEX&color2=HEX2&color3=HEX3&amount=4"
+#define FULL_TO_EMPTY "full_to_empty/color=HEX" // HEX Format: #RRGGBB
 
 //-- END OF DEFAULT FUNCTIONS
 
-#define SET_COLOR "set_color/" //LED_NUMBER + HEX
-#define FILL_COLOR "fill_color/" //HEX + STARTING_LED + COUNT
-#define SET_BRIGHTNESS "set_brightness/" //VALUE
-#define SET_BRIGHTNESS_UNIQUE "set_brightness_unique/" //LED_NUMBER + VALUE
+#define JSON_FUNCTION "json_function/"
+
+#define SET_COLOR "set_color/"                         // LED_NUMBER + HEX
+#define FILL_COLOR "fill_color/"                       // HEX + STARTING_LED + COUNT
+#define SET_BRIGHTNESS "set_brightness/"               // VALUE
+#define SET_BRIGHTNESS_UNIQUE "set_brightness_unique/" // LED_NUMBER + VALUE
 
 /**
  * Starts MQTT server.
  * @param BROKER_URI Broker endpoint.
  * @param AUTH_USR User login authorization.
  * @param AUTH_PWD Password login authorization.
+ * @param numLeds Amount of lamp's leds.
  * @author paproka
  */
-void mqtt_init(char *BROKER_URI, char *AUTH_USR, char *AUTH_PWD);
+void mqtt_init(char *BROKER_URI, char *AUTH_USR, char *AUTH_PWD, int numLeds);
+
+// JSON File structure detailed below
+/*
+    {
+        "leds":
+        {
+            "0":
+            {
+                "c" = "#RRGGBB", <- color in hex format
+                "b" = "50" <- brightness (0-255)
+            },
+            "1":
+            {
+                "c" = "-1" <- Previous color remains
+                "b" = "-1" <- Previous brightness remains
+            },
+            ...
+            "23":
+            {
+                "c" = "#RRGGBB",
+                "b" = "-1"
+            }
+        }
+        "is_flush" = true/false,
+        "is_intermitent" = true/false,
+        "global_b" = "255"
+    }
+
+*/
